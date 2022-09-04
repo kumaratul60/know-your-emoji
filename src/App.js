872ee19a342412c73ee4614baf45fa62429ef6b8
Atul <atulreso1@gmail.com>
2022-09-04 17:19:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+const emojiDictionary = {
+  "😈": "Smiling Face With Horns",
+  "🖖": "Vulcan Salute",
+  "🤑": "Money-Mouth Face",
+  "🔥": "Fire",
+  "🌟": "Glowing Star",
+  "🐘": "Elephant",
+  "🤖": "Robot Face",
+  "🦍": "Gorilla",
+  "🌞": "Sun with Face",
+  "😂": "Face with Tears of Joy",
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "📕": "book",
+  "🖥️": "monitor",
+  "👍": "thumbs-up",
+  "😘": "kiss",
+  "🤐": "shut-up",
+  "👿": "angry",
+};
+const emojiArray = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState("");
+
+  function emojiClickHandler(emoji) {
+    if (emoji in emojiDictionary) {
+      setEmojiMeaning(emojiDictionary[emoji]);
+    }
+  }
+
+  function emojiInputHandler(event) {
+    const input = event.target.value;
+
+    if (input in emojiDictionary) {
+      setEmojiMeaning(emojiDictionary[input]);
+    } else {
+      setEmojiMeaning("Unknown Emoji");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="hero-head">Emoji Interpreter</h1>
+        <div className="emoji-interpreter">
+          <input
+            onChange={emojiInputHandler}
+            className="emoji-input"
+            placeholder="Enter emoji to know it's meaning!"
+          />
+          <div>
+            <h2 className="emoji-meaning">{emojiMeaning}</h2>
+          </div>
+        </div>
+        <div className="known-emojis">
+          <h2>Emojis In Our Database</h2>
+          <div className="known-emojis-list">
+            {emojiArray.map((emoji) => {
+              return (
+                emoji !== "" && (
+                  <div className="emoji-wrapper">
+                    <span
+                      onClick={() => emojiClickHandler(emoji)}
+                      className="emojis"
+                      key={emoji}
+                    >
+                      {emoji}
+                    </span>
+                  </div>
+                )
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
